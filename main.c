c6905630517788a154cec07f67f086a2444e5e89
Sedad1337 <77668127+Sedad1337@users.noreply.github.com>
2021-04-25 16:37:46 +0200

    Pre-work of the GameCountdown function

--- main.c
+++ main.c
@@ -78,3 +78,30 @@ void TimeMeasurement()
 
     printf("The Game was running %.2f seconds\n",totalTime);
 }
+void GameCountdown()
+{
+    clock_t currTime;
+    clock_t endTime;
+
+    int countdownSec = 30;
+
+    /**
+    printf("Bitte Sekunden eingeben ");
+    scanf("%d",&countdownSec);
+    **/
+
+    while(countdownSec!=0)
+    {
+        // Ausgabe
+        printf("Nur noch %d\n",countdownSec);
+        // Ende_Zeit berechnen
+        endTime=clock()+CLOCKS_PER_SEC;
+        do
+        {
+            // Aktuelle_Zeit auslesen
+            currTime=clock();
+        }
+        while(currTime<endTime);
+        // eine Sekunde ist um
+        countdownSec--;
+    }
