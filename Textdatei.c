e6e0d29f27d986eb2b7a22adab2cf5de1683612d
davidnguy <davidnguyen478@gmail.com>
2021-05-03 20:32:46 +0200

    Add Comment to the ImportWordsFromWordlistFile

--- Textdatei.c
+++ Textdatei.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/*This fucntion imports a word from the wordlist.txt file*/
 void ImportWordsFromWordlistFile()
 {
-    //char adfjhnfdj[25];
+
     int counter = 0;
     FILE *inputFile;
     inputFile = fopen("wordlist.txt", "r");
