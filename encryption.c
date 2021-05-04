44da3bf84d2d06c1d83dc7338b4f0b796fe7b920
Sedad1337 <77668127+Sedad1337@users.noreply.github.com>
2021-05-04 23:48:43 +0200

    Reworked encryption.c

--- encryption.c
+++ encryption.c
@@ -1,35 +1,96 @@
-#include<stdio.h>
-#define ROT 13
-
-void encryption()
+#include <stdio.h>
+/*This function exists to encrypt or decrypt thhe wordlist textfiles at choice*/
+int EncryptAndDecrypt()
 {
-    int c,e;
+    char encFile[200];
+    char newencFile[200];
+    char decFile[200];
+    char newdecFile[200];
 
-    while((c=getchar())!=EOF)
+    int choice;
+
+    printf("Enter 1 to Encrypt  / 2 to Decrypt");
+    scanf("%d",&choice);
+
+    switch(choice)
     {
-        if(c >='A' && c <='Z')
-        {
-            if((e = c + ROT) <= 'Z')
-                putchar(e);
-            else
-            {
-                   e = c - ROT;
-                putchar(e);
-            }
-        }
-        else if(c >='a' && c <='z')
-        {
-            if((e= c + ROT) <= 'z')
-                putchar(e);
-            else
-            {
-                e = c - ROT;
-                putchar(e);
-            }
-        }
-        else
-            putchar(c);
+    case 1:
+        printf("Enter the Source Filename:  ");
+        scanf("%s",encFile);
+        printf("Enter the Destination Filename:   ");
+        scanf("%s",newencFile);
+        Encrypt(encFile, newencFile);
+        break;
+    case 2:
+        printf("Enter the Source Filename:   ");
+        scanf("%s",decFile);
+        printf("Enter the Destination Filename:   ");
+        scanf("%s",newdecFile);
+        Decrypt(decFile, newdecFile);
+        break;
     }
+    return 0;
+}
+/*This function will encrypt the wordlist textfile*/
+int Encrypt(char * FILENAME, char * NEW_FILENAME)
+{
+    FILE *inFile;   //Declare inFile
+    FILE *outFile;  //Declare outFile
+    char Byte;
+    char newByte;
+    int n;
+    int i=0;
 
-return 0;
+    inFile = fopen(FILENAME,"rb");
+    outFile = fopen(NEW_FILENAME, "w");
+
+    if(inFile == NULL || outfile == NULL){
+        printf("Error in opening file");
+            return 1;
+    } else {
+        printf("\nFile Opened, Encrypting");
+        while(1){
+                while ( !feof( inFile ) ){
+                    Byte=fgetc(inFile);
+                    newByte=Byte+25;
+                    fputc(newByte,outFile);
+                }
+            printf("End of File");
+            break;
+        }
+        fclose(inFile);
+        fclose(outFile);
+    }
+}
+/*This function will decrypt the wordlist textfile*/
+int Decrypt (char *FILENAME, char *NEW_FILENAME)
+{
+    FILE *inFile; //Declare inFile
+    FILE *outFile; //Declare outFile
+
+    char Byte;
+    char newByte;
+    int i=0;
+
+    inFile = fopen(FILENAME,"rb");
+    outFile = fopen(NEW_FILENAME, "w");
+
+    if(inFile == NULL || outfile == NULL){
+        printf("Error in opening file");
+        return 1;
+    } else {
+        printf("File Opened, Decrypting");
+        while(1){
+            printf(".");
+            while ( !feof( inFile ) ){
+                Byte=fgetc(inFile);
+                newByte=Byte-25;
+                fputc(newByte,outFile);
+            }
+            printf("End of File");
+            break;
+        }
+        fclose(inFile);
+        fclose(outFile);
+    }
 }
